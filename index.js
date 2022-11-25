bba963193890e4f02c87a8a92f3982f11fefdedc
GladysKigen <gladys.kigen@student.moringa.com>
2022-11-26 00:55:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let employee = {
+    name:"", streetAddress:""
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]= value;
+    return employee
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+let copyEmployee = {...employee}//this is how we clone an object
+copyEmployee[key] = value;
+return copyEmployee
+}
+function deleteFromEmployeeByKey(employee,key){
+    let copyEmployee = {...employee}
+    delete copyEmployee[key]
+    return copyEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
